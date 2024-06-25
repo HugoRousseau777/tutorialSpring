fb50ad650827fdc05fa2ef3ea4b0c5aca82e762d
hugo <hugo0703@wanadoo.fr>
2024-06-25 22:35:10 +0200

    MajAutoMinMaxPossiblesFaits

--- ideal_shop/src/app/product-list/product-list.component.ts
+++ ideal_shop/src/app/product-list/product-list.component.ts
@@ -36,6 +36,14 @@ export class ProductListComponent implements OnInit {
   maxFib: number;
 
   maximalCalo!: number;
+  minimalCalo!: number;
+  maximalProt!: number;
+  minimalProt!: number;
+  maximalCarb!: number;
+  minimalCarb!: number;
+  maximalFib!: number;
+  minimalFib!: number;
+
 
   constructor(private productService: ProductService,
     private router: Router
@@ -43,19 +51,119 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit(): void {
     this.getProducts("");
-    this.getMaxCalo("plat");
+    //this.getMaxCalo("plat");
+    this.initMinMax();
   }
 
 
- getMaxCalo(params: string){
-  this.productService.getProductsList(params).subscribe(data => {
-    let maxValue = 0;
-    for (let i=0; i<data.length; i++){
-      if(data[i].pm.calo > maxValue) {
-        maxValue = data[i].pm.calo
+  getProductsList(params: string){
+    return this.productService.getProductsList(params);
+  }
+
+  getProductsListPM(catPlat: string, minCalo: number, maxCalo: number,
+    minCarb: number, maxCarb: number, minProt: number, maxProt: number,
+    minFib: number, maxFib: number
+  ){
+    return this.productService.getProductsListPM(catPlat, minCalo, maxCalo,
+      minCarb, maxCarb, minProt, maxProt,
+      minFib, maxFib);
+  }
+
+  initMinMax(){
+    this.getProductsList("plat").subscribe(data => {
+      let maxCalo = 0;
+      let minCalo = 10000;
+      let maxCarb = 0;
+      let minCarb = 10000; 
+      let maxProt = 0;
+      let minProt = 10000; 
+      let maxFib = 0;
+      let minFib = 10000; 
+      
+      for (let i=0; i<data.length; i++){
+        if(data[i].pm.calo > maxCalo) {
+          maxCalo = data[i].pm.calo
+        }
+        if(data[i].pm.calo < minCalo) {
+          minCalo = data[i].pm.calo
+        }
+        if(data[i].pm.carb > maxCarb) {
+          maxCarb = data[i].pm.carb
+        }
+        if(data[i].pm.carb < minCarb) {
+          minCarb = data[i].pm.carb
+        }
+        if(data[i].pm.prot > maxProt) {
+          maxProt = data[i].pm.prot
+        }
+        if(data[i].pm.prot < minProt) {
+          minProt = data[i].pm.prot
+        }
+        if(data[i].pm.fib > maxFib) {
+          maxFib = data[i].pm.fib
+        }
+        if(data[i].pm.fib < minFib) {
+          minFib = data[i].pm.fib
+        }
+  
       }
+      this.maximalCalo = maxCalo;
+      this.minimalCalo = minCalo;
+      this.maximalCarb = maxCarb;
+      this.minimalCarb = minCarb;
+      this.maximalProt = maxProt;
+      this.minimalProt = minProt;
+      this.maximalFib = maxFib;
+      this.minimalFib = minFib;
+    })
+  }
+
+ getMaxCalo(catPlat: string){
+  this.getProductsListPM(catPlat, this.minCalo, this.maxCalo, this.minCarb, this.maxCarb, this.minProt, this.maxProt, this.minFib, this.maxFib).subscribe(data => {
+    let maxCalo = 0;
+    let minCalo = 10000;
+    let maxCarb = 0;
+    let minCarb = 10000; 
+    let maxProt = 0;
+    let minProt = 10000; 
+    let maxFib = 0;
+    let minFib = 10000; 
+    
+    for (let i=0; i<data.length; i++){
+      if(data[i].pm.calo > maxCalo) {
+        maxCalo = data[i].pm.calo
+      }
+      if(data[i].pm.calo < minCalo) {
+        minCalo = data[i].pm.calo
+      }
+      if(data[i].pm.carb > maxCarb) {
+        maxCarb = data[i].pm.carb
+      }
+      if(data[i].pm.carb < minCarb) {
+        minCarb = data[i].pm.carb
+      }
+      if(data[i].pm.prot > maxProt) {
+        maxProt = data[i].pm.prot
+      }
+      if(data[i].pm.prot < minProt) {
+        minProt = data[i].pm.prot
+      }
+      if(data[i].pm.fib > maxFib) {
+        maxFib = data[i].pm.fib
+      }
+      if(data[i].pm.fib < minFib) {
+        minFib = data[i].pm.fib
+      }
+
     }
-    this.maximalCalo = maxValue;
+    this.maximalCalo = maxCalo;
+    this.minimalCalo = minCalo;
+    this.maximalCarb = maxCarb;
+    this.minimalCarb = minCarb;
+    this.maximalProt = maxProt;
+    this.minimalProt = minProt;
+    this.maximalFib = maxFib;
+    this.minimalFib = minFib;
   })
  }
   
@@ -66,15 +174,18 @@ export class ProductListComponent implements OnInit {
       console.log(data);
     })
   }
+
+  getProductsPm(catPlat: string){
+    this.getProductsListPM(catPlat, this.minCalo, this.maxCalo, this.minCarb, this.maxCarb, this.minProt, this.maxProt, this.minFib, this.maxFib).subscribe(data => {
+      this.products = data;
+      console.log(data);
+      this.getMaxCalo("plat");
+    })
+  }
     details(id: number){
     this.router.navigate(['', id]);
   }
 
-  getProductsListPM(catPlat: string, minCalo: number, maxCalo: number, minCarb: number, maxCarb: number, minProt: number, maxProt: number, minFib: number, maxFib: number){
-    this.productService.getProductsListPM(catPlat, this.minCalo, this.maxCalo, this.minCarb, this.maxCarb, this.minProt, this.maxProt, this.minFib, this.maxFib).subscribe(data => {
-      this.products = data;
-    })
-  }
 
   getProductsSugar(catPlat: string){
     this.productService.getProductsSugar(catPlat).subscribe(data => {
